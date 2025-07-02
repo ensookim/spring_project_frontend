272e5f58e6e43f4012ef4f51f04391fc74e12366
Eunsoo Kim <7.8204620u@gmail.com>
2025-07-02 13:07:51 +0900

    feat: BoardCreatePage

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -7,4 +7,19 @@ export default {
         console.log('BOARD GET LIST: ', data);
         return data;
     },
+    async create(article) {
+        const formData = new FormData();
+        formData.append('title', article.title);
+        formData.append('writer', article.writer);
+        formData.append('content', article.content);
+        if (article.files) {
+            for (let i = 0; i < article.files.length; i++) {
+                formData.append('files', article.files[i]);
+            }
+        }
+        const { data } = await api.post(BASE_URL, formData, { headers });
+        console.log('BOARD POST: ', data);
+        return data;
+    },
+
 }
