41a29caacfb46f981ccb96d48723695cde320ab3
Eunsoo Kim <7.8204620u@gmail.com>
2025-07-02 13:06:09 +0900

    feat: BoardListPage

---
+++ src/api/boardApi.js
@@ -0,0 +1,10 @@
+import api from '@/api';
+const BASE_URL = '/api/board';
+
+export default {
+    async getList(params) {
+        const { data } = await api.get(BASE_URL, { params });
+        console.log('BOARD GET LIST: ', data);
+        return data;
+    },
+}
